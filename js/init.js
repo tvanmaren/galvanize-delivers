4da9052dece621fe8450835c221e2a4e28d43ff6
Tristan Van Maren <tm.vanmaren@gmail.com>
2016-10-22 17:48:43 -0600

    complete cart & menu functionality --missing order submission functionality

--- js/init.js
+++ js/init.js
@@ -1,35 +1,57 @@
 'use strict';
 $(function() {
 
-const $cartTotal=$('#total');
-const $cartTax=$('#tax');
-const $cartSubTotal=$('#subtotal');
-const $cartTable=$('#cartTable');
-const $menu=$('#menu');
+    const $cartTable = $('#cartTable');
+    const $menu = $('#menu');
+    // const $submit = $('#download-button');
+    
+    //ideally we'd want to import our menu cards as an object of objects in a .js file and inject them dynamically//
 
-//ideally we'd want to import our menu cards as an object of objects in a .js file and inject them dynamically//
+    function updateCart() {
+        const $cartTotal = $('#total');
+        const $cartTax = $('#tax');
+        const $cartSubTotal = $('#subtotal');
 
-function updateCart() {
-  console.log('updating cart');
+        var $pricesToAdd = $cartTable.find("td.price");
+        var prices = $pricesToAdd.text().split('$');
+        var newCost = 0;
 
-}
+        for (let i in prices) {
+            if (prices[i]) {
+                newCost += parseFloat(prices[i]);
+            }
+        }
 
-function addToCart(event) {
-  const $thisItem=$(event.target);
-  console.log('you clicked:',$thisItem);
-  var $parentDiv=$thisItem.parent();
-  if ($parentDiv.hasClass('card-action')) {
-    console.log('adding to order');
-    var $cardContent=$parentDiv.siblings('.card-content');
-    var price=$cardContent.children('p').text();
-    var item=$cardContent.children('span').text();
-    var newCartItem='<tr class="addedItem"><td class="item">'+item+'</td><td class="price right">'+price+'</td></tr>';
-    $cartTable.append(newCartItem);
-    return updateCart();
-  }
-  return;
-}
+        $cartSubTotal.text('$' + (newCost.toFixed(2)));
 
-console.log('adding click-listener to',$menu);
-$menu.click(addToCart);
+        $cartTax.text('$' + ((0.1) * parseFloat($cartSubTotal.text().substr(1))).toFixed(2));
+
+        $cartTotal.text('$' + (newCost + parseFloat($cartTax.text().substr(1))).toFixed(2));
+
+    }
+
+    function addToCart(event) {
+        const $thisItem = $(event.target);
+
+        var $parentDiv = $thisItem.parent();
+
+        if ($parentDiv.hasClass('card-action')) {
+            console.log('adding to order');
+            var $cardContent = $parentDiv.siblings('.card-content');
+            var price = $cardContent.children('p').text();
+            var item = $cardContent.children('span').text();
+            var newCartItem = '<tr class="addedItem"><td class="item">' + item + '</td><td class="price right">' + price + '</td></tr>';
+            $cartTable.append(newCartItem);
+            return updateCart();
+        } else {
+            return;
+        }
+    }
+
+    // function submit() {
+    //   console.log($cartTable.text());
+    // }
+
+    $menu.click(addToCart);
+    // $submit.click(submit);
 });
